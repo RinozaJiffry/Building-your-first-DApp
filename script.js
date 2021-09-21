1b065d8099171730da238844115ea88a3efe4dbd
RinozaJiffry <rinoza.20191161@iit.ac.lk>
2021-09-21 22:10:19 +0530

    first DApp

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ const btn = document.getElementById("connect");
 btn.addEventListener('click', ()=>{
     window.web3.currentProvider.enable();
 });
+
+
+web3.eth.getAccounts().then(accounts =>{
+    web3.eth.getBalance(accounts[0]).then(result=>console.log(result))
+});  
